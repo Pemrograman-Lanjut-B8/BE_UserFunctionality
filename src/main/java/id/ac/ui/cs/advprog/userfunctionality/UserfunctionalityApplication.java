414d59b57247264d5e814a9217849786f67feea0
georginaelena <georgina.elena@ui.ac.id>
2024-04-17 07:58:52 +0700

    initialize

---
+++ src/main/java/id/ac/ui/cs/advprog/userfunctionality/UserfunctionalityApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.userfunctionality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserfunctionalityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserfunctionalityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/userfunctionality/UserfunctionalityApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.userfunctionality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserfunctionalityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
