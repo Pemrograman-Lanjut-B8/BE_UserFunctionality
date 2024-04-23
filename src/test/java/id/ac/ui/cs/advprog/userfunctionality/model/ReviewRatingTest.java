a00cce9b3660553f4b1f062e027b390d39ca08f2
novrizair <novrizal.airsyahputra@ui.ac.id>
2024-04-24 04:51:36 +0700

    [RED] Add tests for ReviewRating model

---
+++ src/test/java/id/ac/ui/cs/advprog/userfunctionality/model/ReviewRatingTest.java
@@ -0,0 +1,45 @@
+package id.ac.ui.cs.advprog.userfunctionality.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ReviewRatingTest {
+    private ReviewRating reviewRating;
+
+    @BeforeEach
+    void setUp() {
+        this.reviewRating = new ReviewRating();
+        this.reviewRating.setReviewId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        this.reviewRating.setUsername("novrizair");
+        this.reviewRating.setBookId("123-4567891011");
+        this.reviewRating.setReview("Mantap banget ini buku!");
+        this.reviewRating.setRating(8);
+    }
+
+    @Test
+    void testGetReviewId() {
+        assertEquals("eb558e9f-1c39-460e-8860-71af6af63bd6", this.reviewRating.getReviewId());
+    }
+
+    @Test
+    void testGetUsername() {
+        assertEquals("novrizair", this.reviewRating.getUsername());
+    }
+
+    @Test
+    void testGetBookId() {
+        assertEquals("123-4567891011", this.reviewRating.getBookId());
+    }
+
+    @Test
+    void testGetReview() {
+        assertEquals("Mantap banget ini buku!", this.reviewRating.getReview());
+    }
+
+    @Test
+    void testGetRating() {
+        assertEquals(8, this.reviewRating.getRating());
+    }
+}
