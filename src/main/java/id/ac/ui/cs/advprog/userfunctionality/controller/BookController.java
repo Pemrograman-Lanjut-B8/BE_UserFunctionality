22740a15e81c8fdc600f0c420d05ca5e4e59649d
raihanakbr <muhammad.raihan22@ui.ac.id>
2024-05-24 19:00:52 +0700

    [REFACTOR] Added book search query

--- src/main/java/id/ac/ui/cs/advprog/userfunctionality/controller/BookController.java
+++ src/main/java/id/ac/ui/cs/advprog/userfunctionality/controller/BookController.java
@@ -3,11 +3,9 @@ package id.ac.ui.cs.advprog.userfunctionality.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import id.ac.ui.cs.advprog.userfunctionality.model.Book;
 import id.ac.ui.cs.advprog.userfunctionality.model.builders.BookBuilderImpl;
@@ -34,129 +32,25 @@ public class BookController {
         return ResponseEntity.ok(recommendedBooks);
     }
 
-//    @PostMapping("/book")
-//    public ResponseEntity<?> addBook(
-//            @RequestBody Map<String, String> bookData
-//    ) {
-//        // Check if the request body contains the required fields and if the fields are valid
-//        if (
-//                !bookData.containsKey("judulBuku") ||
-//                        !bookData.containsKey("penulis") ||
-//                        !bookData.containsKey("penerbit") ||
-//                        !bookData.containsKey("deskripsi") ||
-//                        !bookData.containsKey("harga") ||
-//                        !bookData.containsKey("stok") ||
-//                        !bookData.containsKey("isbn") ||
-//                        !bookData.containsKey("jumlahHalaman") ||
-//                        !bookData.containsKey("fotoCover") ||
-//                        !bookData.containsKey("kategori") ||
-//                        !bookData.containsKey("rating") ||
-//                        !bookData.containsKey("tanggalTerbit")
-//        ) {
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-//                    .body("Data buku tidak lengkap");
-//        }
-//
-//        try {
-//            bookBuilder.reset();
-//            bookBuilder.setJudulBuku(bookData.get("judulBuku"));
-//            bookBuilder.setPenulis(bookData.get("penulis"));
-//            bookBuilder.setPenerbit(bookData.get("penerbit"));
-//            bookBuilder.setDeskripsi(bookData.get("deskripsi"));
-//            bookBuilder.setHarga(Double.parseDouble(bookData.get("harga")));
-//            bookBuilder.setStok(Integer.parseInt(bookData.get("stok")));
-//            bookBuilder.setIsbn(bookData.get("isbn"));
-//            bookBuilder.setJumlahHalaman(Integer.parseInt(bookData.get("jumlahHalaman")));
-//            bookBuilder.setFotoCover(bookData.get("fotoCover"));
-//            bookBuilder.setKategori(bookData.get("kategori"));
-//            bookBuilder.setRating(Double.parseDouble(bookData.get("rating")));
-//            bookBuilder.setTanggalTerbit(LocalDate.parse(bookData.get("tanggalTerbit")));
-//            Book addedBook = bookService.createBook(bookBuilder.getBook());
-//            return ResponseEntity.ok(addedBook);
-//        } catch (Exception e) {
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-//                    .body("Gagal membuat buku, format data tidak valid");
-//        }
-//    }
-//
-//    @PutMapping("/book/{isbn}")
-//    public ResponseEntity<?> updateBook(
-//            @PathVariable String isbn,
-//            @RequestBody Map<String, String> bookData
-//    ) {
-//        // Check if the book exists
-//        if (bookService.findByIsbn(isbn) == null) {
-//            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-//                    .body("Buku dengan ISBN " + isbn + " tidak ditemukan");
-//        }
-//
-//        // Check if the request body contains the required fields and if the fields are valid
-//        if (
-//                !bookData.containsKey("judulBuku") ||
-//                        !bookData.containsKey("penulis") ||
-//                        !bookData.containsKey("penerbit") ||
-//                        !bookData.containsKey("deskripsi") ||
-//                        !bookData.containsKey("harga") ||
-//                        !bookData.containsKey("stok") ||
-//                        !bookData.containsKey("jumlahHalaman") ||
-//                        !bookData.containsKey("fotoCover") ||
-//                        !bookData.containsKey("kategori") ||
-//                        !bookData.containsKey("rating") ||
-//                        !bookData.containsKey("tanggalTerbit")
-//        ) {
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-//                    .body("Data buku tidak lengkap");
-//        }
-//
-//        try {
-//            bookBuilder.reset();
-//            bookBuilder.setJudulBuku(bookData.get("judulBuku"));
-//            bookBuilder.setPenulis(bookData.get("penulis"));
-//            bookBuilder.setPenerbit(bookData.get("penerbit"));
-//            bookBuilder.setDeskripsi(bookData.get("deskripsi"));
-//            bookBuilder.setHarga(Double.parseDouble(bookData.get("harga")));
-//            bookBuilder.setStok(Integer.parseInt(bookData.get("stok")));
-//            bookBuilder.setIsbn(isbn);
-//            bookBuilder.setJumlahHalaman(Integer.parseInt(bookData.get("jumlahHalaman")));
-//            bookBuilder.setFotoCover(bookData.get("fotoCover"));
-//            bookBuilder.setKategori(bookData.get("kategori"));
-//            bookBuilder.setRating(Double.parseDouble(bookData.get("rating")));
-//            bookBuilder.setTanggalTerbit(LocalDate.parse(bookData.get("tanggalTerbit")));
-//            Book updatedBook = bookService.update(isbn, bookBuilder.getBook());
-//            return ResponseEntity.ok(updatedBook);
-//        } catch (Exception e) {
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-//                    .body("Gagal mengupdate buku, format data tidak valid");
-//        }
-//    }
-//
-//    @DeleteMapping("/book/{isbn}")
-//    public ResponseEntity<?> deleteBook(
-//            @PathVariable String isbn
-//    ) {
-//        // Check if the book exists
-//        if (bookService.findByIsbn(isbn) == null) {
-//            return ResponseEntity.notFound().build();
-//        }
-//
-//        bookService.deleteByIsbn(isbn);
-//        return ResponseEntity.ok().build();
-//    }
-//
-//    @GetMapping("/book/{isbn}")
-//    public ResponseEntity<?> getBook(
-//            @PathVariable String isbn
-//    ) {
-//        // Check if the book exists
-//        if (bookService.findByIsbn(isbn) == null) {
-//            return ResponseEntity.notFound().build();
-//        }
-//
-//        return ResponseEntity.ok(bookService.findByIsbn(isbn));
-//    }
-//
-//    @GetMapping("/books")
-//    public ResponseEntity<?> getAllBooks() {
-//        return ResponseEntity.ok(bookService.findAll());
-//    }
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/search")
+    public Page<Book> getBooks(
+            @RequestParam(required = false) String judulBuku,
+            @RequestParam(required = false) String penulis,
+            @RequestParam(defaultValue = "tanggalTerbit") String sortBy,
+            @RequestParam(defaultValue = "DESC") String sortDirection,
+            @RequestParam(defaultValue = "1") int pageIndex) {
+        return bookService.searchBooks(judulBuku, penulis, sortBy, sortDirection, pageIndex);
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("/{isbn}")
+    public Book getBookById(@PathVariable String isbn) {
+        return bookService.getBookById(isbn);
+    }
 }
--- src/main/java/id/ac/ui/cs/advprog/userfunctionality/service/BookService.java
+++ src/main/java/id/ac/ui/cs/advprog/userfunctionality/service/BookService.java
@@ -1,12 +1,13 @@
 package id.ac.ui.cs.advprog.userfunctionality.service;
 
 import id.ac.ui.cs.advprog.userfunctionality.model.Book;
+import org.springframework.data.domain.Page;
+
 import java.util.List;
 
 public interface BookService {
-//    List<Book> getAllBooks();
-//    List<Book> getTopRatedBooks();
     List<Book> getBookRecommendation();
-
-
+    public List<Book> getAllBooks();
+    public Page<Book> searchBooks(String judulBuku, String penulis, String sortBy, String sortDirection, int pageIndex);
+    public Book getBookById(String isbn);
 }
